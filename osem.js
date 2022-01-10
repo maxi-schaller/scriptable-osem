57821ba44e3b026ed76e1dd08be91246b29a069e
maxi-schaller <97431065+maxi-schaller@users.noreply.github.com>
2022-01-10 11:16:08 +0100

    Create osem.js

---
+++ osem.js
@@ -0,0 +1,98 @@
+//Einstellungen
+const APIURL = 'https://api.opensensemap.org/boxes/'
+
+
+
+// Abfrage der Widget Parameter
+if(args.widgetParameter){
+  const osemBoxId = args.widgetParameter
+  }
+else {
+  const osemBoxId = '605f244277a88b001ba8d6bb'
+  }
+
+// Festlegen der Datenabfrage URL
+function getOsemApiUrl(boxIdString){
+  return  APIURL + boxIdString
+}
+
+// Fehlerbehandlung
+function fehler(fehlertext){
+  const error = new ListWidget()
+  errorList.addText(felhertext)
+  return null;
+}
+// Laden des Widgets oder von Fehlern
+if(errorList){
+  let iosWidget = errorList
+}
+else{
+  let iosWidget = await createWidget()
+}
+
+if(!config.runsInWidget({
+ await iosWidget.presentSmall() 
+}
+Script.setWidget(iosWidget)
+Script.complete()
+
+
+
+
+// FUNKTIONEN
+
+//// Widget Creator
+async function createWidget(items){
+  //Laden der Daten
+  const senseBoxDaten = await new Request(getOsemApiUrl(osemBoxId)).loadJSON()
+  if(!senseBoxDaten || !senseBoxDaten._id || !senseBoxDaten.name || !senseBoxDaten.sensors){
+    fehler("Keine SenseBox gefunden")
+    return;
+  }
+  //
+  const widgetList = new ListWidget()
+  //Ueberschrift Name der Box
+  const header = widgetList.addText(senseBoxDaten.name)
+  header.font = Font.mediumSystemFont(13)
+  widgetList.addSpacer()
+  //Erstellen der Container
+  const basisContainer = widgetList.addStack()
+  basisConatiner.layoutHorizontally()
+  const linksContainer = basisContainer.addStack()
+  linksContainer.layoutVertically()
+  const mittelSpacer = basisContainer.addSpacer(5)
+  const mittelContainer = basisContainer.addStack()
+  mittelContainer.layoutVertically()
+  const rechtsContainer = basisContainer.addStack()
+  rechtsContainer.layoutVertically()
+  
+  //Schreiben Bezeichnungen
+  senseBoxDaten.sensors.forEach(function(sens){
+    linksContainer.addText(sens.title).font = Font.mediumSystemFont(8)
+    mittelContainer.addText(sens.lastMeasurement.value).font = Font.mediumSystemFont(8)
+    rechtsContainer.addText(sens.unit).font = Font.mediumSystemFont(8)
+  })
+  widgetList.addSpacer()
+  
+  //Zeit der letzten Messung schreiben
+  var letzteMessungZeitpunkt = new Date(senseBoxDaten.sensors[0].lastMeasurement.createdAt)
+  var gestern = new Date()
+  gestern.setDate(gestern.getDate()-1)
+  if(letzteMessungZeitpunkt < gestern){
+    var zeitText = widgetList.addText(letzteMessungZeitpunkt.toLocalString('de-DE'))
+    zeitText.textColor = Color.red()
+  }
+  else {
+   var zeitText = widgetList.addText(letzteMessungZeitpunkt.toLocalString('de-DE', {hour: '2-digit', minute: '2-digit'}))
+   zeitText.textColor = Color.blue()
+  }
+  zeitText.font = Font.newSystemFont(8)
+  
+  //return widgetList
+  
+  
+  
+}
+
+
+
